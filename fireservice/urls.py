89023674ddaa263c2a5d96a650c3cdda4a55dd8c
paxtaeo <paxtaeo@gmail.com>
2021-09-01 15:09:36 +0900

    Add index page

--- fireservice/urls.py
+++ fireservice/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name = 'index'),
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('meal/', include('meal.urls')),
